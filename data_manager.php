cb1f09832b2d1e85f971d1f40cffc7c73d766e44
Rafael Marcos <rafix995@gmail.com>
2021-02-26 11:47:09 +0100

    Metodos de CRUD da classe Funcionario

--- data_manager.php
+++ data_manager.php
@@ -1,7 +1,7 @@
 <?php
     /* Classe Server
     Essa classe abstracta será implementada em todas as classes que irão interagir com a BD*/
-    abstract class Server{
+    class Server{
         protected $c;
         function __construct($host, $user, $password, $db_name, $port){
             try{
@@ -22,21 +22,30 @@
             $stm->bindParam(":id", $id, PDO::PARAM_INT);
             $stm->execute(['id'=>$id]);
 
-            $row = $stm->fetchAll(PDO:FETCH_ASSOC);
+            $row = $stm->fetchAll(PDO::FETCH_ASSOC);
             return $row;
         }
 
         public function getAllFromSala(){
             $q = "SELECT * FROM Sala";
             $stm = $this->c->execute($q);
-            $row = $stm->fetchAll(PDO:FETCH_ASSOC);
+            $row = $stm->fetchAll(PDO::FETCH_ASSOC);
             return $row;
         }
     }
 
     class Encarregado extends Server{
         public function getEncarregadoById($id){
+                   if($this->$c != null){
+                $q = "SELECT * FROM Encarregado WHERE BI_Encarregado = ?;";
+                $stm = $this->c->prepare($q);
+                $stm->bindValue($id, $BI_Encarregado);
+                $stm->execute();
+                $row = $stm->fetch(PDO::FETCH_ASSOC);
 
+                return $row;
+            }
+                return null;
         }
 
         public function getAllFromEncarregado(){
@@ -46,7 +55,16 @@
 
     class Aluno extends Server{
         public function getAlunoById($id){
+           if($this->$c != null){
+                $q = "SELECT * FROM Aluno WHERE BI = ?;";
+                $stm = $this->c->prepare($q);
+                $stm->bindValue($id, $BI);
+                $stm->execute();
+                $row = $stm->fetch(PDO::FETCH_ASSOC);
 
+                return $row;
+            }
+                return null;
         }
 
         public function getAllFromAluno(){
@@ -54,6 +72,9 @@
         }
     }
 
+    /* Classe Funcionário
+    Responsável pelo CRUD da tabela Funcionários na BD asclepio
+    */
     class Funcionario extends Server{
         public function getFuncionarioById($id){
             if($this->$c != null){
@@ -90,13 +111,60 @@
         public function setNome($id, $new_Nome){
             $q = "UPDATE Funcionarios SET Nome_Completo = :new_Nome WHERE BI = :id";
             $stm = $this->$c->prepare($q);
-            $tm->execute(["new_Nome" => $new_Nome, "id" => $id]);
+            $tm->execute(["new_Nome" => $new_Nome, "id" => $id]);// baseasse aqui será sexo,e new_Sexo;
         }
-    }
 
+         public function setSexo($id, $new_Sexo){
+            $q = "UPDATE Funcionarios SET Sexo = :new_Sexo WHERE BI = :id";
+            $stm = $this->$c->prepare($q);
+            $tm->execute(["new_Sexo" => $new_Sexo, "id" => $id]);
+        }
+
+          public function setData_nascimento($id, $new_Data_nascimento){
+            $q = "UPDATE Funcionarios SET Data_nascimento = :new_Data_nascimento WHERE BI = :id";
+            $stm = $this->$c->prepare($q);
+            $tm->execute(["new_Data_nascimento" => $new_Data_nascimento, "id" => $id]);
+        }
+
+         public function setEmail($id, $new_Email){
+            $q = "UPDATE Funcionarios SET Email = :new_Email WHERE BI = :id";
+            $stm = $this->$c->prepare($q);
+            $tm->execute(["new_Email" => $new_Email, "id" => $id]);
+        }
+        public function setTelefone($id, $new_Telefone){
+            $q = "UPDATE Funcionarios SET Telefone = :new_Telefone WHERE BI = :id";
+            $stm = $this->$c->prepare($q);
+            $tm->execute(["new_Telefone" => $new_Telefone, "id" => $id]);
+        }
+
+        public function setFuncao($id, $new_Funcao){
+            $q = "UPDATE Funcionarios SET Funcao = :new_Funcao WHERE BI = :id";
+            $stm = $this->$c->prepare($q);
+            $tm->execute(["new_Funcao" => $new_Funcao, "id" => $id]);
+        }
+
+        public function setSenha($id, $new_Senha){
+            $q = "UPDATE Funcionarios SET Senha = :new_Senha WHERE BI = :id";
+            $stm = $this->$c->prepare($q);
+            $tm->execute(["new_Senha" => $new_Senha, "id" => $id]);
+        }
+}
+
+    /* Classe Comunicado
+    Responsável pelo CRUD na tabela Comunicados na BD asclepio
+    */
     class Comunicado extends Server{
         public function getComunicadoById($id){
+         if($this->$c != null){
+                $q = "SELECT * FROM Comunicado WHERE ID_comunicado = ?;";
+                $stm = $this->c->prepare($q);
+                $stm->bindValue($id, $ID_comunicado);
+                $stm->execute();
+                $row = $stm->fetch(PDO::FETCH_ASSOC);
 
+                return $row;
+            }
+                return null;
         }
 
         public function getAllFromComunidado(){
@@ -104,13 +172,25 @@
         }
     }
 
-    class Boletim extends Server{
+    class Boletins extends Server{
         public function getBoletimById($id){
+                     if($this->$c != null){
+                $q = "SELECT * FROM Boletins WHERE ID_Boletim = ?;";
+                $stm = $this->$c->prepare($q);
+                $stm->bindValue($id, $ID_Boletim);
+                $stm->execute();
+                $row = $stm->fetch(PDO::FETCH_ASSOC);
 
+                return $row;
+            }
+                return null;
         }
 
         public function getAllFromBoletim(){
-
+            $q = "SELECT * FROM Boletins;";
+            $stm = $this->$c->execute($q);
+            $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
+            return $rows;
         }
     }
 
