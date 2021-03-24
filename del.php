1ffb2b5ea074ee2a40073a465bf073593b201e08
Rafael Marcos <rafix995@gmail.com>
2021-03-24 12:40:33 +0100

    Correcção de erro

--- del.php
+++ del.php
@@ -6,12 +6,12 @@
 </head>
 <body>
 
-	
+
 <?php
 
 $BI_coodernador=filter_input(INPUT_GET,'BI_coordenador',FILTER_SANITIZE_NUMBER_INT);
 echo '<h3>Tem certeza que quer excluir este resgistro?</h3>';
-echo "<a href='deletefunc.php?id=$id'>Sim</a>|";
+echo "<a href='deletefunc.php?id=$BI_coodernador'>Sim</a>|";
 echo "<a href='funciconsulta.php'>Não</a>";
 
 
